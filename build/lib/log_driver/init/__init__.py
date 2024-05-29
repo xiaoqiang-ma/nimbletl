e54478ea84a8b4c256ee5f7948780e9d211c4b64
Xiaoqiang Ma <xiaoqiangma@Xiaoqiangs-MacBook-Pro.local>
2024-05-29 15:44:20 +0800

    add .gitignore

--- build/lib/log_driver/init/__init__.py
+++
@@ -1,6 +0,0 @@
-LOG_DRIVE_DDL = """
-    create table xiaoqiang(
-        la int,
-        haa varchar(20)
-    );
-"""
--- build/lib/log_driver/init/init_drive_db.py
+++
@@ -1,6 +0,0 @@
-from common.db import execute_sql
-from log_driver.init import LOG_DRIVE_DDL
-
-
-def initialize_database():
-    execute_sql(LOG_DRIVE_DDL)
